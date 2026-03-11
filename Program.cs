5db8b326a4110d579c418c02fa6eff8c39aaf759
Nils Skäre <nils.skare@outlook.com>
2026-03-11 10:14:43 +0100

    allt gamla

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Discord;
+
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 {
     Args = args,
@@ -6,8 +8,61 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 });
 
 var app = builder.Build();
+app.UseFileServer(); // använd statiska filer
 
-app.UseFileServer();
-app.UseWebSockets();
+static long ToUnixTime(DateTime dt)
+{
+    return ((DateTimeOffset)dt).ToUnixTimeMilliseconds();
+}
+
+var messages = new List<MessageDto>(){
+    new("danne", "hej", ToUnixTime(DateTime.Now.AddDays(-6))),
+    new("lennart", "och hå", ToUnixTime(DateTime.Now.AddDays(-2))),
+    new("xX_Gandalf_Xx", "YOU SHALL NOT POST!", ToUnixTime(DateTime.Now.AddDays(-1))),
+    new("birgitta69", "är nån vaken?", ToUnixTime(DateTime.Now.AddMinutes(-14))),
+    new("danne", "@birgitta69 jo, jag är vaken", ToUnixTime(DateTime.Now.AddMinutes(-11))),
+    new("max", "@danne snacka inte med min brud!", ToUnixTime(DateTime.Now.AddMinutes(-5))),
+};
+
+var globalCts = new CancellationTokenSource();
+
+// Get för meddelanden
+app.MapGet("/api/messages", async (HttpRequest request, CancellationToken ct) =>
+{
+    using var cts = CancellationTokenSource.CreateLinkedTokenSource(globalCts.Token, app.Lifetime.ApplicationStopping, ct);
+
+    if (request.Headers.TryGetValue("X-Poll", out var value) && value == "yes")
+    {
+        try
+        {
+            await Task.Delay(30 * 1000, cts.Token);
+        }
+        catch (TaskCanceledException) { }
+    }
+    return new { messages };
+});
+
+// Post för meddelanden
+app.MapPost("/api/messages", async (MessageDto msg) =>
+{
+    if (string.IsNullOrWhiteSpace(msg.Message))
+        return Results.BadRequest(new { error = "message får inte vara tom." });
+
+    var user = string.IsNullOrWhiteSpace(msg.User) ? "Anonymous" : msg.User.Trim();
+    var message = msg.Message.TrimEnd();
+
+    // Ta fram unixtiden
+    long time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+    Console.WriteLine($"msg post: {msg.User} {time}: {msg.Message}");
+
+    var saved = new MessageDto(user, message, time);
+    messages.Add(saved);
+
+    globalCts.Cancel();
+    globalCts = new CancellationTokenSource();
+
+    return Results.Ok(saved);
+});
 
 app.Run("http://localhost:3000");
